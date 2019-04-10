73ca761164732473c7bb2b007cdb45e168c9034c
vahram87 <vahram.ts@gmail.com>
2019-04-10 21:27:31 +0400

    find css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,13 @@
                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" >
           <div class="carousel-inner">
             <div class="carousel-item active">
-              <img class="d-block w-100" src="{{ URL::asset('storage/juv3.jpg')}}" alt="First slide" style="height: 30rem;">
+              <img class="d-block w-100" src="{{ URL::asset('/storage/juv3.jpg')}}" alt="First slide" style="height: 30rem;">
             </div>
             <div class="carousel-item">
-              <img class="d-block w-100" src="{{ URL::asset('storage/juv1.png')}}" alt="Second slide" style="height: 30rem;">
+              <img class="d-block w-100" src="{{ URL::asset('/storage/juv1.png')}}" alt="Second slide" style="height: 30rem;">
             </div>
             <div class="carousel-item">
-              <img class="d-block w-100" src="{{ URL::asset('storage/juv2.jpg')}}" alt="Third slide" style="height: 30rem;">
+              <img class="d-block w-100" src="{{ URL::asset('/storage/juv2.jpg')}}" alt="Third slide" style="height: 30rem;">
             </div>
           </div>
           <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
